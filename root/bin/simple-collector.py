8f0d94c10031160441ca93dd2d4a2a9dc8c80c6f
altermarkive <altermarkive@gmail.com>
2016-03-08 12:27:19 +0100

    Removed boto dependancy

--- root/bin/simple-collector.py
+++ root/bin/simple-collector.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with code. If not, see http://www.gnu.org/licenses/.
 
-import boto3
-import botocore
 import logging
 import logging.handlers
 import mimetypes
