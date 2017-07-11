af4df4a2fe8df03c10968c8852b0101a51ce40cb
altermarkive <altermarkive@gmail.com>
2017-07-11 19:07:27 +0200

    Added missing Ubuntu packages for t-SNE Python module

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ echo "--- Updating the system ---"
 apt-get -yq update
 
 echo "--- Installing utilities ---"
-apt-get -yq install curl mc imagemagick python3 python3-pip python3-dev python3-tk build-essential subversion git libfreetype6-dev libpng-dev libopenblas-dev
+apt-get -yq install curl mc imagemagick python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev
 
 echo "--- Installing Python modules ---"
 DIRECTORY=$(dirname $0)
-pip3 install --no-cache-dir --upgrade pip -r $DIRECTORY/requirements.txt
+pip3 install --upgrade pip
+pip3 install --no-cache-dir -r $DIRECTORY/requirements.1.txt
+pip3 install --no-cache-dir -r $DIRECTORY/requirements.2.txt
