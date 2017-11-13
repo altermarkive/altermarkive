0d151c2245c104ac92be374c411da21073ab8ae7
altermarkive <altermarkive@users.noreply.github.com>
2017-11-13 20:35:31 +0100

    Added ffmpeg

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "--- Updating the system ---"
 apt-get -yq update
 
 echo "--- Installing utilities ---"
-apt-get -yq install command-not-found curl nano mc imagemagick poppler-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev
+apt-get -yq install command-not-found curl nano mc imagemagick ffmpeg poppler-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev
 
 echo "--- Installing Python modules ---"
 DIRECTORY=$(dirname $0)
