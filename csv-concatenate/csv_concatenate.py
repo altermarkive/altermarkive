6643bfa7dc1e83ae0c06e98a3ac063809fff0fad
altermarkive <altermarkive@gmail.com>
2019-09-19 14:05:55 +0200

    Added csv-concatenate

---
+++ csv-concatenate/csv_concatenate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This concatenates two CSV files.
+"""
+
+import sys
+
+import pandas
+
+
+def main():
+    """
+    Main entry point into the script.
+    """
+    if len(sys.argv) <= 1:
+        print('USAGE: csv_concatenate.py CSV_1 CSV_2 ... CSV_CONCATENATED')
+    else:
+        concatenated = pandas.DataFrame()
+        separate = sys.argv[1:-1]
+        for csv in separate:
+            csv = pandas.read_csv(csv)
+            concatenated = concatenated.append(csv, sort=False)
+        concatenated.to_csv(sys.argv[-1], index=False)
+
+
+if __name__ == '__main__':
+    main()
