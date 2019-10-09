84ef1966cc6aa6e6acf48deb3842a228cf478143
altermarkive <altermarkive@gmail.com>
2019-10-09 19:49:20 +0200

    Made csv-select more resilient to MemoryError

--- csv-select/csv_select.py
+++ csv-select/csv_select.py
@@ -4,6 +4,8 @@
 This script selects columns in CSV file.
 """
 
+import csv
+import operator
 import sys
 
 import pandas
@@ -16,10 +18,18 @@ def main():
     if len(sys.argv) < 4:
         print('USAGE: csv_select.py CSV_FILE_IN CSV_FILE_OUT COLUMN_1 COLUMN_2 ...')  # noqa: E501 pylint: disable=C0301
     else:
-        csv = pandas.read_csv(sys.argv[1])
-        columns = sys.argv[3:]
-        csv = csv[columns].dropna(how='all')
-        csv.to_csv(sys.argv[2], index=False)
+        with open(sys.argv[1], 'r') as handle_in:
+            reader = csv.DictReader(handle_in)
+            columns = sys.argv[3:]
+            values = operator.itemgetter(*columns)
+            with open(sys.argv[2], 'w') as handle_out:
+                writer = csv.DictWriter(handle_out, fieldnames=columns)
+                writer.writeheader()
+                for row in reader:
+                    writer.writerow(dict(zip(columns, values(row))))
+        data = pandas.read_csv(sys.argv[2])
+        data = data[columns].dropna(how='all')
+        data.to_csv(sys.argv[2], index=False)
 
 
 if __name__ == '__main__':
