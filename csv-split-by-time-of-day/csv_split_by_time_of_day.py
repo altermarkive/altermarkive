ce93743e747e706693a8585df8261f5cf26d7665
altermarkive <altermarkive@gmail.com>
2019-10-12 20:41:38 +0200

    Added csv-split-by-time-of-day

---
+++ csv-split-by-time-of-day/csv_split_by_time_of_day.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script splits the value from a given column to separate ones
+depending on time of day (night, morning, afternoon, evening).
+"""
+
+import sys
+import time
+
+import pandas
+
+
+def copy_if_hour_in_range(item, timestamps, column, from_hour, to_hour):
+    """
+    Returns the value from given value if timestamp in given range
+    or Nan otherwise
+    """
+    hour = time.localtime(item[timestamps]).tm_hour
+    in_range = from_hour <= hour < to_hour
+    return item[column] if in_range else float('nan')
+
+
+# pylint: disable-msg=R0913
+def split_off(data, name, applicable, timestamps, column, from_hour, to_hour):
+    """
+    Splits off given column based on hour range
+    """
+    splitted = f'{column} {name}'
+    data[splitted] = float('nan')
+    origin = data[applicable][[timestamps, column]]
+    data.loc[applicable, splitted] = origin.apply(
+        lambda item: copy_if_hour_in_range(
+            item, timestamps, column, from_hour, to_hour), axis=1)
+
+
+def main():
+    """
+    Main entry point into the script.
+    """
+    if len(sys.argv) < 5:
+        print('USAGE: csv_split_by_time_of_day.py CSV_FILE_IN CSV_FILE_OUT TIMESTAMP_COLUMN SPLIT_COLUMN_1 ...')  # noqa: E501 pylint: disable=C0301
+    else:
+        file_in = sys.argv[1]
+        file_out = sys.argv[2]
+        timestamps = sys.argv[3]
+        splitting = sys.argv[4:]
+        data = pandas.read_csv(file_in)
+        applicable = data[timestamps].notnull()
+        for column in splitting:
+            split_off(
+                data, 'Night', applicable, timestamps, column, 0, 6)
+            split_off(
+                data, 'Morning', applicable, timestamps, column, 6, 12)
+            split_off(
+                data, 'Afternoon', applicable, timestamps, column, 12, 18)
+            split_off(
+                data, 'Evening', applicable, timestamps, column, 18, 24)
+        data.to_csv(file_out, index=False)
+
+
+if __name__ == '__main__':
+    main()
