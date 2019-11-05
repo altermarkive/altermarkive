acc48986f753521ec98e4b5f2ee6fdcd91ffff6d
altermarkive <altermarkive@gmail.com>
2019-11-05 21:44:48 +0000

    Added GitHub Actions automation

---
+++ shared/automation/python.linting.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+SELF=$0
+BASE=$(dirname "$0")
+
+pip3 install $(find $BASE/../.. -name "*.requirements.txt" -exec cat {} \; | sort | uniq)
+
+pip3 install bandit==1.6.2 flake8==3.7.8 pylint==2.3.1 pycodestyle==2.5.0
+
+find . -iname "*.py" | xargs pylint --disable=R0801
+pycodestyle .
+flake8 *.py
+bandit -r .
