7b5277fcfd18a50029e2fd0c2a30461665b287e1
altermarkive <altermarkive@gmail.com>
2019-11-06 10:36:42 +0100

    Disabled pip version check

--- shared/automation/lint.here.sh
+++ shared/automation/lint.here.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-pip3 install bandit==1.6.2 flake8==3.7.8 pylint==2.3.1 pycodestyle==2.5.0
+pip3 --disable-pip-version-check install bandit==1.6.2 flake8==3.7.8 pycodestyle==2.5.0 pylint==2.3.1
 
 find . -iname "*.py" | xargs pylint --disable=R0801
 pycodestyle .
