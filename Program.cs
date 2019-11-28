702776bcb2a3c6f9061b41830ccd275aeec35e63
altermarkive <altermarkive@gmail.com>
2019-11-28 11:38:40 +0100

    Created a project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exploring_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# exploring-c-sharp
-Exploring C#
+# Exploring C#
+
+To create this project I ran:
+
+    docker run -it -v $PWD:/project -w /project mcr.microsoft.com/dotnet/core/sdk:3.0 dotnet new console
