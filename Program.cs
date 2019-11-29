aa3cd9f93e1a30891768adc482ff8f3a939c9b07
altermarkive <altermarkive@gmail.com>
2019-11-29 11:21:30 +0100

    Added logging facilities

--- Program.cs
+++ Program.cs
@@ -4,19 +4,40 @@
 namespace Explorer
 {
     using System;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
 
-        /// <summary>
-        /// Main class of the program.
-        /// </summary>
+    /// <summary>
+    /// Main class of the program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Service provider for logging.
+        /// </summary>
+        public static readonly ServiceProvider Provider = InitLogging();
+
         /// <summary>
         /// Main entry into the program.
         /// </summary>
         /// <param name="arguments">Command line arguments of the program.</param>
         public static void Main(string[] arguments)
         {
-            Console.WriteLine("Hello World!");
+            ILogger logger = Program.Provider.GetService<ILogger<Program>>();
+            logger.LogDebug("Hello World!");
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private static ServiceProvider InitLogging()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging(configuration =>
+            {
+                configuration.ClearProviders();
+                configuration.AddConsole();
+            });
+            services.Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Debug);
+            return services.BuildServiceProvider();
         }
     }
 }
