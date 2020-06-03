fdd71b5ab9cd090af9f1318db5b9b0fbc7f89b67
altermarkive <altermarkive@users.noreply.github.com>
2020-06-03 16:00:48 +0200

    Reinstated Docker installation but removed failing command-not-found

--- install.sh
+++ install.sh
@@ -10,11 +10,21 @@ echo "--- Updating the system ---"
 apt-get -yq update
 
 echo "--- Installing utilities ---"
-apt-get -yq install apt-transport-https ca-certificates gnupg-agent software-properties-common command-not-found curl zip nano mc imagemagick ffmpeg poppler-utils libgxps-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev jq gettext nmap
+apt-get -yq install apt-transport-https ca-certificates gnupg-agent software-properties-common curl zip nano mc imagemagick ffmpeg poppler-utils libgxps-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev jq gettext nmap
 sed -i '/PDF/d' /etc/ImageMagick-6/policy.xml
 if [ "$#" -eq 1 ]; then
     apt-get -yq install ntpdate
     ntpdate 0.pool.ntp.org
+fi
+
+echo "--- Installing Docker ---"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $CODENAME stable" | tee /etc/apt/sources.list.d/docker-ce.list
+apt-get -yq update
+apt-get -yq install docker-ce docker-ce-cli containerd.io
+if [ "$#" -eq 1 ]; then
+    groupadd docker 2> /dev/null || true
+    /bin/sh -c "usermod -aG docker $1"
 fi
 
 echo "--- Installing Azure CLI ---"
