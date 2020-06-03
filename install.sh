f240a86f3e95c7a3f4fe66d47db39c0732a6dc7f
altermarkive <altermarkive@users.noreply.github.com>
2020-06-03 15:45:23 +0200

    Updating the Docker installation

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "--- Updating the system ---"
 apt-get -yq update
 
 echo "--- Installing utilities ---"
-apt-get -yq install apt-transport-https ca-certificates software-properties-common command-not-found curl zip nano mc imagemagick ffmpeg poppler-utils libgxps-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev jq gettext nmap
+apt-get -yq install apt-transport-https ca-certificates gnupg-agent software-properties-common command-not-found curl zip nano mc imagemagick ffmpeg poppler-utils libgxps-utils python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev libopenblas-dev libblas-dev libatlas-base-dev jq gettext nmap
 sed -i '/PDF/d' /etc/ImageMagick-6/policy.xml
 if [ "$#" -eq 1 ]; then
     apt-get -yq install ntpdate
@@ -21,7 +21,7 @@ echo "--- Installing Docker ---"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $CODENAME stable" | tee /etc/apt/sources.list.d/docker-ce.list
 apt-get -yq update
-apt-get -yq install docker-ce
+apt-get -yq install docker-ce docker-ce-cli containerd.io
 if [ "$#" -eq 1 ]; then
     groupadd docker 2> /dev/null || true
     /bin/sh -c "usermod -aG docker $1"
