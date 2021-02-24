531771c7ba8f6b66f1cd0975494c4714080e5002
altermarkive <altermarkive@gmail.com>
2021-02-24 14:21:44 +0100

    Decoupled retroactive-git

--- workspace/install.sh
+++ workspace/install.sh
@@ -10,7 +10,7 @@ echo "--- Updating the system ---"
 apt-get -yq update
 
 echo "--- Installing utilities ---"
-apt-get -yq install apt-transport-https ca-certificates gnupg-agent software-properties-common curl zip python3 python3-pip python3-dev python3-tk build-essential git libfreetype6-dev libpng-dev gettext nmap netcat-openbsd tcpdump
+apt-get -yq install apt-transport-https ca-certificates gnupg-agent software-properties-common curl zip python3 python3-pip python3-dev python3-tk build-essential libfreetype6-dev libpng-dev gettext nmap netcat-openbsd tcpdump
 
 echo "--- Installing Azure CLI ---"
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $CODENAME main" | tee /etc/apt/sources.list.d/azure-cli.list
@@ -21,17 +21,6 @@ apt-get -yq install azure-cli
 echo "--- Installing Python modules ---"
 DIRECTORY=$(dirname $0)
 pip3 install --no-cache-dir -r $DIRECTORY/requirements.txt
-
-echo "--- Installing date/time enforcement ---"
-
-git clone --branch v0.9.8 https://github.com/wolfcw/libfaketime.git /tmp/libfaketime
-cd /tmp/libfaketime
-make install
-cd -
-rm -rf /tmp/libfaketime
-echo "Example use:"
-echo "  LD_PRELOAD=/usr/local/lib/faketime/libfaketime.so.1 FAKETIME_NO_CACHE=1 FAKETIME='1970-01-01 00:00:00' date"
-echo "  LD_PRELOAD=/usr/local/lib/faketime/libfaketime.so.1 FAKETIME_NO_CACHE=1 FAKETIME='+365d' date"
 
 echo "--- Clean-up ---"
 apt-get -yq autoremove
