59ea3462627e094a4fe981aea310919f9d5e039b
altermarkive <altermarkive@gmail.com>
2021-05-20 18:18:22 +0200

    Corrected ssh-jump-server utility

--- ssh-jump-server/entrypoint.sh
+++ ssh-jump-server/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa &> /dev/null
 fi
 if [ ! -f "/etc/ssh/ssh_host_ecdsa_key" ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+	ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa &> /dev/null
 fi
 if [ ! -f "/etc/ssh/ssh_host_ed25519_key" ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
-fi
-
-if [ ! -d "/var/run/sshd" ]; then
-    mkdir -p /var/run/sshd
+	ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 &> /dev/null
 fi
 
 exec "$@"
