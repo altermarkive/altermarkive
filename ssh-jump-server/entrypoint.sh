e4f0938fece9978953ce0ae0b9f6e14c11809fe2
altermarkive <altermarkive@gmail.com>
2021-05-20 19:08:29 +0200

    Resolved access rights issue for ssh-jump-server utility

--- ssh-jump-server/entrypoint.sh
+++ ssh-jump-server/entrypoint.sh
@@ -10,4 +10,8 @@ if [ ! -f "/etc/ssh/ssh_host_ed25519_key" ]; then
 	ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 &> /dev/null
 fi
 
+cp /config/authorized_keys /home/user/.ssh/authorized_keys
+chmod 0600 /home/user/.ssh/authorized_keys
+chown 1000.100 /home/user/.ssh/authorized_keys
+
 exec "$@"
