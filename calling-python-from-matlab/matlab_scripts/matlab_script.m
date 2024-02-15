1265a329d4f716646b9de256686586132f2d096e
altermarkive <altermarkive@users.noreply.github.com>
2024-02-15 11:50:22 +0100

    Decoupled into https://stackoverflow.com/a/40006108/5223524

--- calling-python-from-matlab/matlab_scripts/matlab_script.m
+++
@@ -1,7 +0,0 @@
-[own_path, ~, ~] = fileparts(mfilename('fullpath'));
-module_path = fullfile(own_path, '..');
-python_path = py.sys.path;
-if count(python_path, module_path) == 0
-    insert(python_path, int32(0), module_path);
-end
-py.our_module.our_script.our_function('Hello')
--- calling-python-from-matlab/our_module/our_script.py
+++
@@ -1,4 +0,0 @@
-import numpy
-
-def our_function(text):
-    print('%s %f' % (text, numpy.nan))
