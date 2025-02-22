af1b0da7e24d4f917820354e85af7edb8f4286e5
altermarkive <altermarkive@gmail.com>
2025-02-22 21:56:53 +0100

    Corrections

--- other/sovreign-compute/docker/entrypoint.sh
+++ other/sovreign-compute/docker/entrypoint.sh
@@ -6,7 +6,7 @@ USER=user
 
 if [ -n "$USER_ID" ] && [ "$USER_ID" -ne 0 ]; then
   groupadd --gid $GROUP_ID $USER
-  useradd --uid $USER_ID --gid $GROUP_ID --create-home $USER
+  useradd --uid $USER_ID --gid $GROUP_ID $USER
   echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
   chmod 0440 /etc/sudoers.d/$USER
   exec sudo -H -u $USER -- "$@"
