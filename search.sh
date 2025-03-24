59909f61b436c94bc314f60de4c8124c199c60dd
altermarkive <altermarkive@gmail.com>
2025-03-24 12:43:10 +0000

    Better search

--- search.sh
+++ search.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-EXTENSIONS=$1
-PATTERN=$2
+SEARCH_EXTENSIONS=$1
+SEARCH_TERM=$2
 
-GLOBS=$(for extension in ${EXTENSIONS//,/ }; do echo -n " --glob '*.$extension'"; done)
-
-SELECTION=$(eval rg --line-number --no-heading --color=never $GLOBS "'"$PATTERN"'" . \
-    | fzf --delimiter ':' --preview 'tail -n +{2} {1}' \
-    | awk -F: '{ print $1, $2 }' \
-    | nano +$1 $0)
-
-echo $SELECTION
+rg --line-number --no-heading --color=never $(echo "$SEARCH_EXTENSIONS" | tr ',' '\n' | sed 's/^/--glob=*./g') "$SEARCH_TERM" \
+| fzf --delimiter ':' --preview 'tail -n +{2} {1}' \
+| awk -F: '{ print $1, $2 }' \
+| xargs nano +
