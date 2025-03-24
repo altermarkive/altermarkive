adb5dc6383fec6230f82cff78d27ac46b6087bc0
altermarkive <altermarkive@gmail.com>
2025-03-24 12:56:20 +0000

    Now search.sh works as intended

--- search.sh
+++ search.sh
@@ -3,7 +3,8 @@
 SEARCH_EXTENSIONS=$1
 SEARCH_TERM=$2
 
-rg --line-number --no-heading --color=never $(echo "$SEARCH_EXTENSIONS" | tr ',' '\n' | sed 's/^/--glob=*./g') "$SEARCH_TERM" \
-| fzf --delimiter ':' --preview 'tail -n +{2} {1}' \
-| awk -F: '{ print $1, $2 }' \
-| xargs nano +
+SEARCH_RESULT=$(rg --line-number --no-heading --color=never $(echo "$SEARCH_EXTENSIONS" | tr ',' '\n' | sed 's/^/--glob=*./g') "$SEARCH_TERM" \
+  | exec fzf --delimiter ':' --preview 'tail -n +{2} {1}' \
+  | awk -F: '{ print $2, $1 }')
+
+vi +$SEARCH_RESULT
