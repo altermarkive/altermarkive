0020accab8dd35492a16d9c6c9fc777e983a77ed
altermarkive <altermarkive@gmail.com>
2025-05-09 11:13:34 +0200

    Improved code search script

--- other/sovreign-compute/docker/tools/search.py
+++ other/sovreign-compute/docker/tools/search.py
@@ -4,14 +4,15 @@ import os
 import sys
 
 
-if len(sys.argv) < 3:
-    sys.exit(1)
-search_extensions = sys.argv[1]
-search_term = sys.argv[2]
-command = f"""
-vi +$(rg --line-number --no-heading --color=never $(echo '{search_extensions}' | tr ',' '\n' | sed 's/^/--glob=*./g') '{search_term}'
-| exec fzf --delimiter ':' --preview 'tail -n +{{2}} {{1}}'
-| awk -F: '{{ print $2, $1 }}')
-"""
-command = command.replace('\n', '')
-os.system(f'/bin/sh -c "{command}"')
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit(1)
+    search_extensions = ' '.join([f'--glob=*.{extension}' for extension in sys.argv[1].split(',')])
+    search_term = sys.argv[2]
+    command = f"""
+    vi +$(rg --line-number --no-heading --color=never {search_extensions} '{search_term}'
+    | exec fzf --delimiter ':' --preview 'tail -n +{{2}} {{1}}'
+    | awk -F: '{{ print $2, $1 }}')
+    """
+    command = command.replace('\n', '')
+    os.system(f'/bin/sh -c "{command}"')
