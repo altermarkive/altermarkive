daa325a5c98c0f807da4d23eb2c488be72899d64
altermarkive <altermarkive@users.noreply.github.com>
2025-10-20 20:55:36 +0200

    Simpler PDF password handling

--- sovereign/privileged/roles/utilities/files/pdf_encrypt.py
+++ sovereign/privileged/roles/utilities/files/pdf_encrypt.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
-import getpass
 import os
 import sys
 from pathlib import Path
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         sys.exit(1)
-    password = getpass.getpass(prompt='Password: ')
-    password_repeated = getpass.getpass(prompt='Password (repeat): ')
-    if password == password_repeated:
-        pdf_file = Path(sys.argv[1])
-        password = f'{pdf_file.stem}{password}'
-        original_pdf_file = pdf_file.parent / f'_{pdf_file.name}'
-        encrypted_pdf_file = pdf_file
-        pdf_file.rename(original_pdf_file)
-        os.system(f'qpdf --encrypt {password} {password} 256 -- {original_pdf_file} {encrypted_pdf_file}')
+    password = sys.argv[2]
+    pdf_file = Path(sys.argv[1])
+    original_pdf_file = pdf_file.parent / f'{pdf_file.stem}_{pdf_file.suffix}'
+    encrypted_pdf_file = pdf_file
+    pdf_file.rename(original_pdf_file)
+    os.system(f'qpdf --encrypt {password} {password} 256 -- {original_pdf_file} {encrypted_pdf_file}')
