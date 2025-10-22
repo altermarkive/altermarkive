11197dd2f9fb134b6c590382e0813bf7773088bc
altermarkive <altermarkive@gmail.com>
2025-10-22 18:33:58 +0200

    Changed how output PDF is named to ease sorting

--- sovereign/privileged/roles/utilities/files/pdf_optimize.py
+++ sovereign/privileged/roles/utilities/files/pdf_optimize.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit(1)
     pdf_file = Path(sys.argv[1])
-    original_pdf_file = pdf_file.parent / f'_{pdf_file.name}'
+    original_pdf_file = pdf_file.parent / f'{pdf_file.stem}_{pdf_file.suffix}'
     optimized_pdf_file = pdf_file
     pdf_file.rename(original_pdf_file)
     os.system(f'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile={optimized_pdf_file} {original_pdf_file}')
